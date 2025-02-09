473a1467e6299609518c24a1b081d8f4cd9297f6
Computhink-CS127 <cs127@computhink.com.sg>
2025-02-09 08:30:15 +0000

    Feb 9, 2025, 8:30 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,1 +1,4 @@
-Name = input()
+Name = input("What is your friend's name?")
+Age = input("How old is your friend this year?")
+Msg = input("What is your message to your friend?")
+Print("Happy " + Age + "th" + " Birthday" + Name + ("!") + Message)                  
