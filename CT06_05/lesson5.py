3b5dc6a0790786b9a146666e9fa1b0b07d9512fe
Computhink-CS127 <cs127@computhink.com.sg>
2025-02-09 09:06:14 +0000

    Feb 9, 2025, 9:06 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -2,8 +2,12 @@
 # Age = input("How old is your friend this year?")
 # Message = input("What is your message to your friend?")
 # if last_digit == 1:
-?
+#             suffix = "st"
+#         elif last_digit == 2:
+#             suffix = "nd"
+#         elif last_digit == 3:
+#             suffix = "rd"
 
-# print("Happy " + Age + "th Birthday " + Name + "!" + Message)
+# # print("Happy " + Age + "th Birthday " + Name + "!" + Message)
 for i in range(1,5):
     print(i)
