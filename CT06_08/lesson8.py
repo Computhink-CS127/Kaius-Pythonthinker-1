0803d09944cc79861253f31e4a9c5ee727655cb8
Computhink-CS127 <cs127@computhink.com.sg>
2025-03-02 08:38:55 +0000

    Mar 2, 2025, 8:38 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -3,4 +3,4 @@ ans = 1
 for i in range(1,6):
     num = int(input("what is #" + str(i) + "?"))
 ans = ans * num
-print("The product of the five numbers is " + product)
+print("The product of the five numbers is " + ans)
