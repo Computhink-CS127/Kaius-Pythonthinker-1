0b27dc126a8f3927b739ff757f3ea4519c946aed
Computhink-CS127 <cs127@computhink.com.sg>
2025-03-02 08:52:21 +0000

    Mar 2, 2025, 8:52 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,6 +1,6 @@
 
 
-for i in range(1,6):
-    num = int(input("what is #" + str(i) + "?"))
-ans = ans * num
-print("The product of the five numbers is " + ans)
+# for i in range(1,6):
+#     num = int(input("what is #" + str(i) + "?"))
+# ans = ans * num
+# print("The product of the five numbers is " + ans)
