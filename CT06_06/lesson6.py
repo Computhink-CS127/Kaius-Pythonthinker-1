cb591771bd11e049adde95c8cf7c91bdad9aafa8
Computhink-CS127 <cs127@computhink.com.sg>
2025-03-02 07:56:33 +0000

    Mar 2, 2025, 7:56 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,1 +1,53 @@
-print("Hello from lesson 6")
+# no_of_students = int(input("How many students are there?"))
+# total = 0
+# for i in range(no_of_students):
+#     score = int(input("What did the student get?"))
+#     total = score + total
+
+# print("The average score is " + str(total/no_of_students))
+
+
+
+for i in range(3):
+    print("Hello, World!")
+
+
+for i in range(5):
+    print(i)
+
+
+print("Hello, World!")
+
+
+fi = 5
+
+
+age = 2
+print(age)
+
+name = "Alice"
+print(name)
+
+x = 5
+print(x)
+
+print("Hello, World!")
+
+
+age = "25"
+print(int(age) + 1)
+
+number = 10
+print(number - 5)
+
+print(Repeat * 3)
+
+str(year = 2023)
+print("The year is " + year)
+
+x = 10
+y = x / 2
+
+end = 5
+for i in range(end):
+    print(i)
--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,2 +1,38 @@
-print("Hello from lesson 7")
+# score_one = 80
+# score_two = 90
+# score_three = 75
 
+# total = score_one + score_two + score_three
+
+# average_score = total / 3
+
+# student_name = "Alex"
+
+# print("Average score for " + student_name + " is: " + str(average_score))
+
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(2,21,2):
+#     print(i)
+
+# for i in range(10,0,-1):
+#     print(i)
+
+# word = input("what is the word?")
+# n = int(input("how many times would you like to repeat it?"))
+# for i in range(n):
+#     print(word)
+
+# sum = 0
+
+# for i in range(1,6):
+#     num = int(input("What is number #" + str(i) + "? "))
+#     sum = num + sum
+
+# print("The sum of the numbers is:" + str(sum))
+
+num = int(input("What is the number?"))
+
+for i in range(1,13):
+    print(str(num) + "x" + str(i) + "=" + str(num*i))
