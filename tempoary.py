e9a29287cda896ac8bf212f75e0c93a336357b4e
Computhink-CS127 <cs127@computhink.com.sg>
2025-03-09 08:43:17 +0000

    Mar 9, 2025, 8:43 AM

--- tempoary.py
+++ tempoary.py
@@ -1,6 +1,7 @@
-# Ask for the start,end and 
+# Ask for the start,end and step numbers
 start = int(input("What is the starting number?"))
 end = int(input("What is the ending number?"))
 step = int(input("What is the step?"))
+
 for i in range(start,end,step):
     print (i)
