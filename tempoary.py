a9ae50f19891a8c6f6db078c03cc7b45b914e3de
Computhink-CS127 <cs127@computhink.com.sg>
2025-03-09 08:40:07 +0000

    Mar 9, 2025, 8:40 AM

--- tempoary.py
+++ tempoary.py
@@ -2,4 +2,4 @@ start = int(input("What is the starting number?"))
 end = int(input("What is the ending number?"))
 increment = int(input("What is the increment?"))
 for i in range(start,end,increment):
-    print
+    print (i)
