e3ef7c22705f1c21f37d9532d0fbaa18fe346f51
Computhink-CS127 <cs127@computhink.com.sg>
2025-03-30 08:14:04 +0000

    Mar 30, 2025, 8:14 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,6 +1,6 @@
 px = int(input("Ehat is the price of that toilet seat? $"))
 
-if px < 5:
+if px <= 5:
     print("Ur so broke man")
 elif px < 50:
     print("Are you sure you need this ultrasonic portable golden encased toilet seat?")
