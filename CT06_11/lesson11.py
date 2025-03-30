28ae5f237a29413b7dc8dfcca0d3f0e77b53503a
Computhink-CS127 <cs127@computhink.com.sg>
2025-03-30 08:28:23 +0000

    Mar 30, 2025, 8:28 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,11 +1,13 @@
-input("What do you wnt to purchase on this very fine sunny joyful perfect beautiful zesty energetic day?")
-px = int(input("What is the price of that toilet seat? $"))
+# input("What do you wnt to purchase on this very fine sunny joyful perfect beautiful zesty energetic day?")
+# px = int(input("What is the price of that toilet seat? $"))
 
-if px <= 5:
-    print("Ur so broke man")
-elif px <= 50:
-    print("Are you sure you need this ultrasonic portable golden encased toilet seat?")
-elif px <= 500:
-    print("Payment accepted!Pls note that your bank account value is -9,999,999,999")
-else:
-    print("Bro is Mr Beast")
+# if px <= 5:
+#     print("Ur so broke man")
+# elif px <= 50:
+#     print("Are you sure you need this ultrasonic portable golden encased toilet seat?")
+# elif px <= 500:
+#     print("Payment accepted!Pls note that your bank account value is -9,999,999,999")
+# else:
+#     print("Bro is Mr Beast")
+
+
