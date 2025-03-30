5dbe1422ec53b5245ef4ff7adce2c41b56272cad
Computhink-CS127 <cs127@computhink.com.sg>
2025-03-30 09:21:36 +0000

    Mar 30, 2025, 9:21 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -50,4 +50,4 @@ password = input("What is your password John? ")
 if Username == "John123" and password == "pw123":
     print("Access granted.")
 elif Username == "John123" or password == "pw123":
-    
+    print("Either your ")
