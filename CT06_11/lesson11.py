74c062963fbc13b9dc78f0c99e2a1af8fe280af0
Computhink-CS127 <cs127@computhink.com.sg>
2025-03-30 08:15:31 +0000

    Mar 30, 2025, 8:15 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -5,4 +5,4 @@ if px <= 5:
 elif px <= 50:
     print("Are you sure you need this ultrasonic portable golden encased toilet seat?")
 elif px <= 500:
-    print("Payment accepted!Pls note that your bank account value is -999")
+    print("Payment accepted!Pls note that your bank account value is -9,999,999,999")
