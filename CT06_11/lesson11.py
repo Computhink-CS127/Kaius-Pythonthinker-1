9fcfb47fcf0e074f53476f45de3a53575df3c29d
Computhink-CS127 <cs127@computhink.com.sg>
2025-03-30 08:18:41 +0000

    Mar 30, 2025, 8:18 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,4 +1,4 @@
-input("What do you wnt to purchase on this very fine sunny joy")
+input("What do you wnt to purchase on this very fine sunny joyful perfect ")
 px = int(input("What is the price of that toilet seat? $"))
 
 if px <= 5:
