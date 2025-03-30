a9d2ab49dc8fcd1d78c51f1b682748bde362710f
Computhink-CS127 <cs127@computhink.com.sg>
2025-03-30 08:32:32 +0000

    Mar 30, 2025, 8:32 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -11,9 +11,13 @@
 #     print("Bro is Mr Beast")
 
 
-num = int(input("Key in a number: "))
+# num = int(input("Key in a number: "))
 
-if num % 3 == 0 and num % 7 == 0:
-    print("You are a CHOSEN ONE!!!")
-else:
-    print("Bro you suck at guessing numbers.")
+# if num % 3 == 0 and num % 7 == 0:
+#     print("You are a CHOSEN ONE!!!")
+# else:
+#     print("Bro you suck at guessing numbers.")
+
+
+
+
