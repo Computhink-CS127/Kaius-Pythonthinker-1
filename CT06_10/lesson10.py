e166b94d413ffd2d2328e82d88fb09eede1117c7
Computhink-CS127 <cs127@computhink.com.sg>
2025-03-30 07:57:05 +0000

    Mar 30, 2025, 7:57 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,68 @@
-print("Hello from lesson 10")
+# num = int(input("Key in a number: "))
+
+# if num > 0:
+#     print(str(num) + " is a positive number.")
+# else:
+#     print(str(num) + " is a negative number.")
+
+# task 2
+# import random
+
+# a = random.randint(1,10)
+# b = int(input("Guess the number from  1 to 10!"))
+# if a == b:
+#     print("So you were not dumb.")
+# else:
+#     print("Try again you failure.")
+
+
+# task 3
+
+# password = int(696)
+# guess = int(input("Guess the 3 digit password: "))
+
+# if guess == password:
+#     print("No way you must be a hacker.GET OUT!!!!!!!!!!")
+# else:
+#     print("Womp womp you suck and got it WRONG.")
+
+
+
+# task 4
+# num = int(input("Key in a number: "))
+
+# if num % 2 == 1:
+#     print("This number is odd.")
+# else:
+#     print("This number is even.")
+
+# Task 5
+# age = int(input("What is your age bro? "))
+
+# if age < 0:
+#     print("Congrats you are officialy unborn.")
+# elif age < 13:
+#     print("Still a toddler buddy.")
+# elif age < 19:
+#     print("You are a rebellious teenager")
+# elif age < 100:
+#     print("bro is aging.")
+# else:
+#     print("Dying soon bro I'll be at your funeral.")
+
+
+# Task 9
+score = int(input("What did you score in the Cambridge-Oxford intellectual and genuine passing test for non-failure students? "))
+
+if score < 60:
+    print("FAILURE!!!!!!!!!!!!!!!!!!")
+elif score < 70:
+    print("Just passed only by luck lah.")
+elif score < 80:
+    print("Okay-okay I guess.")
+elif score < 90:
+    print("You are better than average.")
+elif score < 100:
+    print("You are a top student.")
+else:
+    print("HACKER ALERT!!!!!!!!!!!!!!!!")
