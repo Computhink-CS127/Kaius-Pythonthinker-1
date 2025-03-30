2a681eef56c4e3e7e51b808276afb13bce871fd4
Computhink-CS127 <cs127@computhink.com.sg>
2025-03-30 08:14:35 +0000

    Mar 30, 2025, 8:14 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -4,4 +4,5 @@ if px <= 5:
     print("Ur so broke man")
 elif px <= 50:
     print("Are you sure you need this ultrasonic portable golden encased toilet seat?")
-eli
+elif px <= 500:
+    print("")
