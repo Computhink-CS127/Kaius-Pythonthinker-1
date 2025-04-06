cb3bf1142cb283df5f8527e1e9290f19bc147138
Computhink-CS127 <cs127@computhink.com.sg>
2025-04-06 08:25:47 +0000

    Apr 6, 2025, 8:25 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -10,9 +10,24 @@
 #     If true, print "The number is divisible by 3 and 5!"
 # 3. Else, print "The number is not divisible by 3 and 5"
 
-num = int(input("Key in a number: "))
+# num = int(input("Key in a number: "))
 
-if num % 3 == 0 and num % 5 == 0:
-    print("The number is divisible by 3 and 5!")
-else:
-    print("The numberis not divisible by 3 and 5!")
+# if num % 3 == 0 and num % 5 == 0:
+#     print("The number is divisible by 3 and 5!")
+# else:
+#     print("The numberis not divisible by 3 and 5!")
+
+
+# Task 1: Introduction to while loop
+# **Task 1a**:
+# Due to a pandemic, the government placed a limit to the number
+# of visitors a venue can have.
+
+# Using a 'while' loop, create a program that will increase the
+# number of visitors by 1 before printing out the number of
+# visitors admitted, until number of visitors reaches 50.
+
+# 1. Create a 'visitors' variable and assign '0' to it
+# 2. While there is less than 50 visitors,
+#     I. Increase the visitor count by 1
+#     II. Print the visitor count
