f4321406ca09a9d32f07e73912949ff09b8dc893
Computhink-CS127 <cs127@computhink.com.sg>
2025-04-06 09:17:35 +0000

    Apr 6, 2025, 9:17 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -99,12 +99,33 @@
 # 1. Modify your code to remove the comma (",") that appears
 #    either at the start or end of your sentence
 
-order = "Nothing"
+# order = "Nothing"
 
-while True:
-    items = input("What do you want to buy?")
-    if items == "end":
-        print("Your order is:" + order + " and " + items)
-        break
-    order = "Your order is:" + order + " and " + items
-    print(order)
+# while True:
+#     items = input("What do you want to buy?")
+#     if items == "end":
+#         print("Your order is:" + order + " and " + items)
+#         break
+#     order = "Your order is:" + order + " and " + items
+#     print(order)
+
+
+# Task 4: while... else
+# The else condition will run when the loop exits normally.
+# i.e. when the while condition is no longer true.
+
+# Using 'else', create a program that will count from 10 to 1,
+# before printing "Happy New Year!"
+
+# **Task 4a**:
+# Using a 'while' loop:
+# 1. Print the numbers from 10 to 1
+# 2. Once count reaches 1, use 'else' to print
+#    "Happy New Year!"
+
+# **Task 4b**:
+# Now, modify your 'while' loop from Task 4a such that:
+# 1. If the number is 5, 'break' out of the loop
+# 2. Ensure your code has an 'else'
+
+# Observe that "Happy New Year!" is no longer printed
