327a2f47d833419bca983b5fa5dae28f68e14cbe
Computhink-CS127 <cs127@computhink.com.sg>
2025-04-06 08:16:06 +0000

    Apr 6, 2025, 8:16 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -15,4 +15,4 @@ num = int(input("Key in a number: "))
 if num % 3 == 0 and num % 5 == 0:
     print("The number is divisible by 3 and 5!")
 else:
-    print("The numberis no")
+    print("The numberis not divisible by 3 and 5")
