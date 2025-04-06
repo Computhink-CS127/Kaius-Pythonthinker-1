9936f96ae52919f78a20543707e5ac86bcfcdd53
Computhink-CS127 <cs127@computhink.com.sg>
2025-04-06 09:07:17 +0000

    Apr 6, 2025, 9:07 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -103,7 +103,7 @@ order = ""
 
 while True:
     items = input("What do you want to buy?")
-    order = "Your order is:" + order + " and " + items
-    print(order)
     if items == "end":
         break
+    order = "Your order is:" + order + " and " + items
+    print(order)
