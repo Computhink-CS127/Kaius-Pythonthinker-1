f6b82467430d6cd2898090e0f3196d4e0598fb85
Computhink-CS127 <cs127@computhink.com.sg>
2025-04-27 08:54:36 +0000

    Apr 27, 2025, 8:54 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -50,13 +50,13 @@
 
 
 
-# import turtle
+import turtle
 
-# window = turtle.Screen()
-# window.setup(width=600,height=400)
-# t = turtle.Turtle()
-# t.down()
-# t.shape("triangle")
-# t.fillcolor("white")
-# window.mainloop()
+window = turtle.Screen()
+window.setup(width=600,height=400)
+t = turtle.Turtle()
+t.down()
+t.shape("triangle")
+t.fillcolor("white")
+window.mainloop()
 
