c4d2b918b18fc1f6a75a015c7768fd2798c4682d
Computhink-CS127 <cs127@computhink.com.sg>
2025-05-18 08:38:42 +0000

    May 18, 2025, 8:38 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -3,5 +3,7 @@ def setup(screenWidth,screenHeight):
     window = turtle.Screen
     window.setup(width = screenWidth,height = screenHeight)
     return window
+
+
     t = turtle.Turtle
     window.mainloop()
